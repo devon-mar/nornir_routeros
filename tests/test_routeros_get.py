09a6326e5e13a1d08119ea239825bd7d7d534638
Devon Mar <devonm@mdmm.ca>
2021-11-12 23:35:06 -0800

    Add test with name

--- tests/test_routeros_get.py
+++ tests/test_routeros_get.py
@@ -12,3 +12,16 @@ def test_routeros_get(nr):
     assert device_result.changed is False
     assert "routerboard" in device_result.result[0]
     assert device_result.result[0]["routerboard"] == "false"
+
+
+def test_get_with_name(nr):
+    result = nr.run(
+        task=routeros_get,
+        path="/ip/service",
+        name_="www"
+    )
+    assert len(result["router1"]) == 1
+    host_result = result["router1"][0]
+    assert host_result.failed is False
+    assert host_result.changed is False
+    assert host_result.result[0]["name"] == "www"
